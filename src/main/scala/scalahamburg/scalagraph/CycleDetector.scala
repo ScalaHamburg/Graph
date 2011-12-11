91f09e48c11d6cdbfe89e7d4efe198382a1df955
PT <pt@someplace.com>
2011-12-11 15:30:04 +0100

    Mit externem comparator

--- src/main/scala/scalahamburg/scalagraph/CycleDetector.scala
+++ src/main/scala/scalahamburg/scalagraph/CycleDetector.scala
@@ -4,29 +4,29 @@ import scalax.collection.GraphEdge._
 import scalax.collection.GraphPredef._
 import scalax.collection.Graph
 import scala.collection.Set
+import scala.math.Ordered
 
 // TODO Path benutzen
-// TODO Cycles "croppen"
-class CycleDetector(g: Graph[Int, DiEdge]) {
+class CycleDetector[N](g: Graph[N, DiEdge], comparator:(N,N)=>Boolean) {
 
-  def findCycles(): Set[List[Int]] = {
-    var visitedNodes = Set[Int]()
-    var cycles = Set[List[Int]]()
+  def findCycles(): Set[List[N]] = {
+    var visitedNodes = Set[N]()
+    var cycles = Set[List[N]]()
     for (node <- g.nodes) {
       if (!visitedNodes.contains(node.value)) {
         deepthFirstFrom(List(node.value), visitedNodes) match {
           case (cyc, vNodes) => cycles = cycles ++ cyc; visitedNodes = visitedNodes ++ vNodes
-          case _ =>
+          case _             =>
         }
       }
     }
     cycles
   }
 
-  def deepthFirstFrom(path: List[Int], visitedNodes: Set[Int]): (Set[List[Int]], Set[Int]) = {
+  def deepthFirstFrom(path: List[N], visitedNodes: Set[N]): (Set[List[N]], Set[N]) = {
     val start = path.first
     val successors = g.find(start).get.diSuccessors
-    var cycles = Set[List[Int]]();
+    var cycles = Set[List[N]]();
 
     if (successors.isEmpty) {
       return (cycles, visitedNodes)
@@ -34,22 +34,22 @@ class CycleDetector(g: Graph[Int, DiEdge]) {
 
     var newVisitedNodes = visitedNodes
     successors.foreach { n =>
-      val value: Int = n.value
+      val value: N = n.value
       newVisitedNodes = newVisitedNodes + value
-        if (path.contains(value)) { // loop detected
-          // take only nodes in the Cycle
-          val cropped = path.take(path.indexOf(value)+1).sort(_<_)
-          val firstInCycle = g.get(cropped.first)
-          val nextInCycle = firstInCycle.diSuccessors.filter(n=>cropped.contains(n.value))
-          val cPath = nextInCycle.first.shortestPathTo(firstInCycle).get.nodes
-          cycles = cycles + cPath.map(_.value)
-        } else {
-          val newPath = n.value :: path
-          val (c, newAll) = deepthFirstFrom(newPath, newVisitedNodes)
-          if (!c.isEmpty) {
-            cycles = cycles ++ c
-          }
+      if (path.contains(value)) { // loop detected
+        // take only nodes in the Cycle
+        val cropped = path.take(path.indexOf(value) + 1).sort(comparator)
+        val firstInCycle = g.get(cropped.first)
+        val nextInCycle = firstInCycle.diSuccessors.filter(n => cropped.contains(n.value))
+        val cPath = nextInCycle.first.shortestPathTo(firstInCycle).get.nodes
+        cycles = cycles + cPath.map(_.value)
+      } else {
+        val newPath = n.value :: path
+        val (c, newAll) = deepthFirstFrom(newPath, newVisitedNodes)
+        if (!c.isEmpty) {
+          cycles = cycles ++ c
         }
+      }
     }
     (cycles, newVisitedNodes)
   }
@@ -59,14 +59,14 @@ class CycleDetector(g: Graph[Int, DiEdge]) {
 object CycleDetector {
   def main(args: Array[String]) {
 
-    val gWithCycle = Graph(1 ~> 2) + (2 ~> 3) + (3 ~> 4) + (4 ~> 5) + (4 ~> 2)
+    val gWithCycle = Graph("1" ~> "2") + ("2" ~> "3") + ("3" ~> "4") + ("4" ~> "5") + ("4" ~> "2")
 
-    val cd = new CycleDetector(gWithCycle)
+    val cd = new CycleDetector(gWithCycle, ((a:String, b:String)=>a<b))
 
     println(cd.findCycles())
 
-    val gWithMoreCycles = Graph(1 ~> 2) + (2 ~> 3) + (3 ~> 4) + (4 ~> 5) + (5 ~> 6) + (6 ~> 7) + (7 ~> 8) + (8 ~> 9) + (8 ~> 10) + (10 ~> 11) + (11 ~> 12) + (12 ~> 5) + (12 ~> 10)    
-    val cd2 = new CycleDetector(gWithMoreCycles)
+    val gWithMoreCycles = Graph(1 ~> 2) + (2 ~> 3) + (3 ~> 4) + (4 ~> 5) + (5 ~> 6) + (6 ~> 7) + (7 ~> 8) + (8 ~> 9) + (8 ~> 10) + (10 ~> 11) + (11 ~> 12) + (12 ~> 5) + (12 ~> 10)
+    val cd2 = new CycleDetector(gWithMoreCycles,  ((a:Int, b:Int)=>a<b))
     println(cd2.findCycles())
 
   }
